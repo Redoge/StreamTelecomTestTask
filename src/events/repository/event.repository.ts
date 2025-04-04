948c06fdc4cc5cbbee022da2cd4425ea3e193c14
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-04 21:39:23 +0300

    feat(events): add event repository for database interactions
    - Created EventRepository class for handling event-related database operations
    - Implemented create method to insert call events into the database
    - Added findByUserCallerIdAndDateRange method to retrieve events for a specific user
    - Implemented findAll method to fetch events within a given date range
    - Utilized DatabaseService for executing database queries
    - Added error logging for database operation failures

---
+++ src/events/repository/event.repository.ts
@@ -0,0 +1,72 @@
+import {Injectable, Logger} from "@nestjs/common";
+import {DatabaseService} from "../../database/database.service";
+import {EventEntity} from "../entity/event.entity";
+
+@Injectable()
+export class EventRepository {
+    private readonly logger = new Logger(EventRepository.name);
+
+    constructor(private readonly databaseService: DatabaseService,) {
+    }
+
+    async create(event: EventEntity) {
+        try {
+            const query = `
+                INSERT INTO call_events (event_type, channel, caller_id_num, connected_line_num,
+                                         caller_id, recipient_id,
+                                         unique_id, linked_id, cause, cause_txt, context, exten,
+                                         channel_state, channel_state_desc, timestamp, raw_data)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+            `;
+
+            await this.databaseService.executeQuery(query, [
+                event.eventType,
+                event.channel,
+                event.callerIdNum,
+                event.connectedLineNum,
+                event.callerId,
+                event.recipientId,
+                event.uniqueId,
+                event.linkedId,
+                event.cause,
+                event.causeTxt,
+                event.context,
+                event.exten,
+                event.channelState,
+                event.channelStateDesc,
+                event.timestamp,
+                event.rawData
+            ]);
+        } catch (err) {
+            this.logger.error(`Error saving call event: ${err}`);
+        }
+    }
+
+    async findByUserCallerIdAndDateRange(userCallerId: string, startDate: Date, endDate: Date) {
+        try {
+            const query =
+                `SELECT *
+                 FROM call_events
+                 WHERE (caller_id = $1 OR recipient_id = $1)
+                   AND timestamp BETWEEN $2 AND $3;`
+
+            return await this.databaseService.executeQuery(query, [userCallerId, startDate, endDate]);
+        } catch (err) {
+            this.logger.error(`Error fetching call events for user: ${err}`);
+            throw err;
+        }
+    }
+    async findAll(startDate: Date, endDate: Date) {
+        console.log({startDate, endDate})
+        try {
+            const query =
+                `SELECT * FROM call_events
+                 WHERE timestamp BETWEEN $1 AND $2;`
+
+            return await this.databaseService.executeQuery(query, [startDate, endDate]);
+        } catch (err) {
+            this.logger.error(`Error fetching call events for user: ${err}`);
+            throw err;
+        }
+    }
+}
