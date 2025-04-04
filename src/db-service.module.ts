71592392bd8c114bd84a5a0583061d7638ced4b3
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-04 21:39:04 +0300

    feat(module): create db-service module
    - Added new DbServiceModule with configuration for RabbitMQ, Database, and Auth modules
    - Imported ConfigModule to manage environment configurations
    - Set up module imports with no additional providers

---
+++ src/db-service.module.ts
@@ -0,0 +1,17 @@
+// db-service/src/app.module.ts
+import { Module } from '@nestjs/common';
+import { RabbitMqModule } from './rabbitmq/rabbitmq.module';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    RabbitMqModule,
+    DatabaseModule,
+    AuthModule,
+  ],
+  providers: [],
+})
+export class DbServiceModule {}
