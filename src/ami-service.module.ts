e980d2524447c41ce3f77ee1a0f60e8a415196a3
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-04 21:38:15 +0300

    feat(module): create AmiServiceModule with initial configuration
    - Added new module for AMI service
    - Imported ConfigModule for environment configuration
    - Integrated AmiModule and RabbitMqModule as core dependencies

---
+++ src/ami-service.module.ts
@@ -0,0 +1,14 @@
+// ami-service/src/app.module.ts
+import { Module } from '@nestjs/common';
+import { AmiModule } from './ami/ami.module';
+import { RabbitMqModule } from './rabbitmq/rabbitmq.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    AmiModule,
+    RabbitMqModule,
+  ],
+})
+export class AmiServiceModule  {}
